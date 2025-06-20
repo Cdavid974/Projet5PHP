e31457294152cda832c2569cf16600b03c4129dc
David CAVANILLAS <114383991+Cdavid974@users.noreply.github.com>
2025-06-20 13:37:36 +0400

    modified:   header.php 	modified:   oeuvres.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ArtBox</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <a href="index.php"><img src="./images/logo.png" alt="mon logo"></a>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
---
+++ oeuvres.php
@@ -0,0 +1,13 @@
+<?php
+$oeuvres = [
+    '0' => [
+        'id' => '1',
+        'title' => '',
+        'img' => '',
+        'alt' => '',
+        'author' => '',
+        'description' => '',
+        'enabled' => true,
+
+    ]
+];
