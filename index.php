7afbddaca197a2747552859b551c9ca9265eab59
David CAVANILLAS <114383991+Cdavid974@users.noreply.github.com>
2025-06-20 14:14:05 +0400

    renamed:    images/fly-d.png -> fly-d.png 	modified:   index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     ?>
     <main>
         <h2>Mon titre</h2>
-        <img src="./images/fly-d.png" alt="fly">
+        <img src="fly-d.png" alt="fly">
         <p>
             Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu (domicile en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.",
             'enabled' => true,
